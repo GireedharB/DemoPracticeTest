bbadc0324708f5be98ddf4cdfd614cc2b82ae301
GireedharB <gireedharbirajdar1011@gmail.com>
2024-12-02 12:33:00 +0530

    Demo project added

---
+++ src/main/java/com/qa/demo/basedata/DemoBase.java
@@ -0,0 +1,12 @@
+package com.qa.demo.basedata;
+
+import org.testng.annotations.Test;
+
+public class DemoBase {
+	@Test
+	
+	public void BaseData() {
+		System.out.println("This Demo base data");
+	}
+
+}
---
+++ src/test/java/com/qa/demotestcases/DemoFirstTest.java
@@ -0,0 +1,15 @@
+package com.qa.demotestcases;
+
+import org.testng.annotations.Test;
+
+public class DemoFirstTest {
+	@Test
+	public void TC1() {
+		System.out.println("This is Demo first testcase");
+	}
+	@Test
+	public void TC2() {
+		System.out.println("This is demo second testcase");
+	}
+
+}
